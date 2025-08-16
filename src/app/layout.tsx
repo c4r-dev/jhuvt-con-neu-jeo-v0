171bd93152b83d44ecc19690ac9d7e0c45479eaf
David Fischer <fiscd@seas.upenn.edu>
2025-08-16 13:55:02 -0400

    commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import Image from 'next/image';
+import "./globals.css";
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  const handleResetClick = () => {
+    window.scrollTo({ top: 0, behavior: 'auto' });
+    window.location.href = '/';
+  };
+
+  return (
+    <html lang="en">
+
+      <head>
+        <title>NEUROSERPIN</title>
+        <meta name="description" content="NEUROSERPIN" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
+
+      <body>
+        <header className="header"> {/* Header class from globals.css */}
+          <button
+            className="favicon-button" // Favicon button class
+            onClick={handleResetClick}
+            title="Reset Application"
+          >
+            <Image
+              src="/favicon.ico"
+              alt="Logo - Reset"
+              width={40}
+              height={40}
+              className="favicon" // Favicon class
+              priority
+            />
+          </button>
+          <div className="title-container"> {/* Title container class */}
+            <h1 className="title">NEUROSERPIN</h1> {/* Title class */}
+          </div>
+        </header>
+
+        <main>{children}</main>
+
+      </body>
+
+    </html>
+  );
+}
